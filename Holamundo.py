2576241fa502ffa8050ae66c2b970a6d7827c38a
JuanManuelEsquivel <67649143+JuanManuelEsquivel@users.noreply.github.com>
2020-07-03 16:19:35 -0500

    Ejemplos

---
+++ Holamundo.py
@@ -0,0 +1,26 @@
+#if __name__ == "__main__" :
+#print('hola')
+#opcion=input("Ingresa la opción 1.- Sumar 2.- Restar")
+#x=input("Dame el primer valor")
+#y=input("Dame el segundo valor")
+#if opcion in '1':
+#    z=x+y
+#    print("El resultado es: ", z)
+#elif opcion in '2':
+#    print("El resultado es: ", x-y)
+#else:
+#    print("Elegiste otra opción")
+
+
+#tupla= 3,4,5,x,y,'hola'
+#print(tupla[:])
+lista= []
+
+diccionario ={'nombre':'Juan','apellido':'Porra','numero_de_medidor':'879'}
+lista.append(diccionario)
+diccionario1 ={'nombre':'Manuel','apellido':'Euivel','numero_de_medidor':'880'}
+lista.append(diccionario1)
+print(lista[0]['apellido'])
+
+
+
